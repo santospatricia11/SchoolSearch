b15c078cd197fb363c1fc55e5c73f07badbbfed5
Filipe Farias <filipe.farias.chagas@hotmail.com>
2021-11-24 12:52:16 -0300

    modificações de ontem do

---
+++ src/main/java/com/aps/schoolsearch/SecurityConfiguration.java
@@ -0,0 +1,30 @@
+package com.aps.schoolsearch;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+	
+	@Bean 
+	public PasswordEncoder passwordEncoder() { 
+	    return new BCryptPasswordEncoder(); 
+	}
+	@Override
+	protected void configure(final AuthenticationManagerBuilder auth) throws Exception {
+	    auth.inMemoryAuthentication().withUser("user")
+	    	.password(passwordEncoder().encode("pass")).authorities("USER");
+	}
+	
+	@Override
+	protected void configure(final HttpSecurity http) throws Exception {
+	    http.authorizeRequests().anyRequest().authenticated().and().formLogin();//.loginPage("/login").permitAll();
+	}
+}
