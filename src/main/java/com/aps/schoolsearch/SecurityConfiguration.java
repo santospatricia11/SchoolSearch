bbfedb3a21dfe2bba8fabb70a4c75fa063cdc136
Filipe Farias <filipe.farias.chagas@hotmail.com>
2021-11-24 18:03:07 -0300

    adicionado o header/fragment

--- src/main/java/com/aps/schoolsearch/SecurityConfiguration.java
+++ src/main/java/com/aps/schoolsearch/SecurityConfiguration.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -22,9 +23,15 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 	    auth.inMemoryAuthentication().withUser("user")
 	    	.password(passwordEncoder().encode("pass")).authorities("USER");
 	}
+	@Override
+	public void configure(WebSecurity web) throws Exception {
+		web
+	      .ignoring()
+	         .antMatchers("/resources/**"); 
+	}
 	
 	@Override
 	protected void configure(final HttpSecurity http) throws Exception {
-	    http.authorizeRequests().anyRequest().authenticated().and().formLogin();//.loginPage("/login").permitAll();
+		http.authorizeRequests().anyRequest().authenticated().and().formLogin();//.loginPage("/login").permitAll();
 	}
 }
