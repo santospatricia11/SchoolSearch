004a61eb92bb7abf10a328f869936302cf3327c4
Filipe Farias <filipe.farias.chagas@hotmail.com>
2021-11-30 23:33:04 -0300

    adição de parte da tela de cadastro de usuários.

--- src/main/java/com/aps/schoolsearch/controller/UsuarioController.java
+++ src/main/java/com/aps/schoolsearch/controller/UsuarioController.java
@@ -9,6 +9,7 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.aps.schoolsearch.model.Usuario;
 import com.aps.schoolsearch.repository.UsuarioRepository;
@@ -19,6 +20,8 @@ public class UsuarioController {
 	@Value("${spring.application.name}")
 	private String appName;
 	
+	private static final String cadastroUsuario = "cadastro-usuario";
+	
 	@Autowired
 	private UsuarioRepository usuarioRepository;
 	
@@ -26,18 +29,23 @@ public class UsuarioController {
 		model.addAttribute("app_name", appName);
 	}
 	
-	@GetMapping("/add")
-	public String usuarioForm(Model model) {
+	private String formularioCadastro(Model model) {
 		setModel(model);
 		model.addAttribute("form_name", "Cadastro de Usuário no"+appName);
 		model.addAttribute("usuario", new Usuario());
-		return "usuario-form";
+		return cadastroUsuario;
 	}
+	@GetMapping("/cadastrar-usuario")
+	@RequestMapping("/cadastrar-usuario")
+	public String cadastroUsuario(Model model) {
+		return formularioCadastro(model);
+	}
+
 	
-	@PostMapping("/process")
+	@PostMapping("/processar")
 	public String processForm(@Valid Usuario usuario, BindingResult result) {
 		if(result.hasErrors()) {
-			return "usuario-form";
+			return cadastroUsuario;
 		}
 		usuarioRepository.save(usuario);
 		return "redirect:/";
--- src/main/java/com/aps/schoolsearch/model/Usuario.java
+++ src/main/java/com/aps/schoolsearch/model/Usuario.java
@@ -1,11 +1,14 @@
 package com.aps.schoolsearch.model;
 
-import java.time.LocalDate;
-
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
+
+import org.springframework.format.annotation.DateTimeFormat;
 
 import lombok.EqualsAndHashCode;
 
@@ -15,20 +18,34 @@ import lombok.EqualsAndHashCode;
 public class Usuario {
 	
 	@NotNull
+	@NotEmpty(message="Nome não pode ser vazio")
+	@Min(value=3, message="Nome deve ter no mínimo 3 caracteres")
 	private String nome;
 	
 	@Id
-	@Pattern(regexp="^\\d{3}.\\d{3}.\\d{3}-\\d{2}$")
+	@Column(unique=true)
+	@NotEmpty(message="O campo CPF não pode ser vazio")
+	@Pattern(regexp="^\\d{3}.\\d{3}.\\d{3}-\\d{2}$", message="Digite um CPF válido, padrão: ___.___.___-__")
 	private String cpf;
+	
+	
+	@Column(unique=true)
+	@NotNull
+	@NotEmpty
+	@Pattern(regexp="^[a-z0-9.]+@[a-z0-9]+\\.[a-z]+\\.([a-z]+)?$", message="Digite um email válido, padrão: _@_._")
+	private String email;
 	
 	@NotNull
 	private String endereco;
 	
 	@NotNull
+	@Column(unique=true)
+	@Pattern(regexp="^(\\d{2})(9.)?\\d{4}-\\d{4}", message="Digite um telefone válido, padrão (__)(9.)____-____")
 	private String telefone;
 	
-	@NotNull
-	private LocalDate dataNascimento;
+	@NotNull(message="Digite/Escolha uma data de nascimento válida, mínimo 18 anos, máximo 01/01/1900")
+	@DateTimeFormat(pattern="dd/MM/yyyy")
+	private String dataNascimento;
 	
 	@NotNull
 	private Boolean pne;
@@ -37,7 +54,8 @@ public class Usuario {
 	private String sexo;
 	
 	@NotNull
-	private String nomeUsuario;
+	@NotEmpty(message="A senha não pode ser vazia")
+	private String senha;
 	
 	public String getNome() {
 		return nome;
@@ -63,10 +81,10 @@ public class Usuario {
 	public void setTelefone(String telefone) {
 		this.telefone = telefone;
 	}
-	public LocalDate getDataNascimento() {
+	public String getDataNascimento() {
 		return dataNascimento;
 	}
-	public void setDataNascimento(LocalDate dataNascimento) {
+	public void setDataNascimento(String dataNascimento) {
 		this.dataNascimento = dataNascimento;
 	}
 	public Boolean getPne() {
@@ -81,11 +99,17 @@ public class Usuario {
 	public void setSexo(String sexo) {
 		this.sexo = sexo;
 	}
-	public String getNomeUsuario() {
-		return nomeUsuario;
+	public String getSenha() {
+		return senha;
 	}
-	public void setNomeUsuario(String nomeUsuario) {
-		this.nomeUsuario = nomeUsuario;
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
 	}
 	
 	
