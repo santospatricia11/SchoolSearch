0c240f3964041fb835f6829c8494f22b6fcd7634
Patricia <patricia.pereira@academico.ifpb.edu.br>
2021-12-12 23:20:23 -0300

    Alterações na tela lista-escola

--- src/main/java/com/aps/schoolsearch/controller/ListarEscolaComtroller.java
+++ src/main/java/com/aps/schoolsearch/controller/ListarEscolaComtroller.java
@@ -3,8 +3,13 @@ package com.aps.schoolsearch.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
+import com.aps.schoolsearch.model.Escola;
+import com.aps.schoolsearch.repository.EscolaRepository;
 import com.aps.schoolsearch.repository.UsuarioRepository;
 @Controller
 
@@ -13,7 +18,8 @@ public class ListarEscolaComtroller {
 	private String appName;
 	
 	@Autowired
-	private UsuarioRepository usuarioRepository;
+	private EscolaRepository escolaRepository;
+	
 	
 
 }
